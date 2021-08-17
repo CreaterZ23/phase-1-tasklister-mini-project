557799370e3c8e2d9492c4d73e1447ab726b4434
Zeon <zeonstewart23@gmail.com>
2021-08-16 20:13:32 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    document.getElementById('create-task-form').addEventListener('submit', (s) => {
+      s.preventDefault()
+      buildToDoItem(s.target.querySelector("input").value)
+    })
 });
+
+function buildToDoItem (doThis) {
+  const li = document.createElement('li')
+  const x = document.createElement('button')
+  x.addEventListener('click', (e) =>{
+    e.target.parentNode.remove()
+  })
+  li.textContent = ` ${doThis} `
+  x.textContent = " X " 
+  li.appendChild(x)
+  document.getElementById('tasks').appendChild(li)
+
+}
